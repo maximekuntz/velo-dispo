8f56253e05fc941844b53ffcef1891b6e0ee5c0e
Maxime Kuntz <maxime.kuntz75@gmail.com>
2024-12-03 22:22:40 +0100

    Support translations for names and add new cities
    Networks added: Agen, Bordeaux, Brest, Carcassonne, Lille, Niort, Tarbes, La Bresse - Gérardmer, Valenciennes and Vichy

--- app.py
+++ app.py
@@ -8,12 +8,22 @@ GBFS_URLS = {
     "Paris et communes limitrophes": "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/gbfs.json",
     "Lyon": "https://download.data.grandlyon.com/files/rdata/jcd_jcdecaux.jcdvelov/gbfs.json",
     "Marseille": "https://api.omega.fifteen.eu/gbfs/2.2/marseille/en/gbfs.json?&key=MjE0ZDNmMGEtNGFkZS00M2FlLWFmMWItZGNhOTZhMWQyYzM2",
+    "Agen": "https://api.gbfs.ecovelo.mobi/tempovelo/gbfs.json",
+    "Bordeaux": "https://bdx.mecatran.com/utw/ws/gbfs/bordeaux/v3/gbfs.json?apiKey=opendata-bordeaux-metropole-flux-gtfs-rt",
+    "Brest": "https://gbfs.partners.fifteen.eu/gbfs/2.2/brest/en/gbfs.json",
+    "Carcassonne": "https://api.gbfs.ecovelo.mobi/cyclolibre/gbfs.json",
+    "Lille": "https://media.ilevia.fr/opendata/gbfs.json",
     "Mulhouse": "https://api.cyclocity.fr/contracts/mulhouse/gbfs/gbfs.json",
     "Nancy": "https://api.cyclocity.fr/contracts/nancy/gbfs/gbfs.json",
     "Nantes": "https://api.cyclocity.fr/contracts/nantes/gbfs/gbfs.json",
+    "Niort": "https://api.gbfs.ecovelo.mobi/tanlib/gbfs.json",
     "Saint-Brieuc": "https://gateway.prod.partners-fs37hd8.zoov.site/gbfs/2.2/saintbrieuc/en/gbfs.json?key=YmE1ZDVlNDYtMGIwNy00MGEyLWIxZWYtNGEwOGQ4NTYxNTYz",
     "Strasbourg": "https://gbfs.nextbike.net/maps/gbfs/v2/nextbike_ae/gbfs.json",
+    "Tarbes": "https://api.gbfs.ecovelo.mobi/tlpmobilites/gbfs.json",
     "Epinal": "https://gbfs.partners.fifteen.eu/gbfs/epinal/gbfs.json",
+    "La Bresse Gérardmer": "https://api.gbfs.v3.0.ecovelo.mobi/labresse/gbfs.json",
+    "Valenciennes": "https://stables.donkey.bike/api/public/gbfs/2/donkey_valenciennes/gbfs",
+    "Vichy": "https://gbfs.partners.fifteen.eu/gbfs/vichy/gbfs.json",
 }
 
 st.set_page_config(
@@ -23,7 +33,9 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 st.title(":bike: Vélo Dispo | Trouver un vélo en libre-service")
-st.write("Bienvenue sur Vélo Dispo :bike:, l'application qui vous permet de trouver un vélo en libre-service à proximité de votre position.")
+st.write(
+    "Bienvenue sur Vélo Dispo :bike:, l'application qui vous permet de trouver un vélo en libre-service à proximité de votre position."
+)
 
 
 @st.cache_data
@@ -31,8 +43,11 @@ def get_feed(url: str):
     response = requests.get(url)
     r = response.json()
     data = r["data"]
+    if "feeds" in data:
+        return data["feeds"]
     language_key = list(data.keys())[0]
     return data[language_key]["feeds"]
+
 
 def get_specific_feed(feeds: dict, feed_name: str):
     for feed in feeds:
@@ -40,14 +55,31 @@ def get_specific_feed(feeds: dict, feed_name: str):
             return feed["url"]
     raise ValueError(f"No '{feed_name}' feed found")
 
+
 def get_system_information_feed(feeds: dict):
     return get_specific_feed(feeds, "system_information")
 
+
+def get_language_text(texts: list[dict] | str, language: str | None = None):
+    if type(texts) is str:
+        print("Not a list, returning value.")
+        return texts
+
+    if language is None:
+        return texts[0]["text"]
+    for name in texts:
+        if name["language"] == language:
+            return name["text"]
+    raise ValueError(f"No text found for language '{language}'")
+
+
 def get_station_information_feed(feeds: dict):
-    return get_specific_feed(feeds, "station_information")    
+    return get_specific_feed(feeds, "station_information")
+
 
 def get_station_status_feed(feeds: dict):
     return get_specific_feed(feeds, "station_status")
+
 
 with st.sidebar:
     city = st.selectbox("Choisir une ville", list(GBFS_URLS.keys()))
@@ -66,7 +98,7 @@ with st.sidebar:
     station_status = requests.get(station_status_url).json()
     station_status_df = pd.DataFrame(station_status["data"]["stations"])
 
-    st.metric("Réseau", system_information["data"]["name"])
+    st.metric("Réseau", get_language_text(system_information["data"]["name"]))
     st.metric("Nombre de stations", len(station_information["data"]["stations"]))
 
     st.divider()
@@ -75,11 +107,15 @@ with st.sidebar:
     operator = system_information.get("data").get("operator")
     operator_msg = f" sont fournies par {operator} et" if operator else ""
     st.write(f"Les données{operator_msg} sont mises à jour toutes les {system_information['ttl']} secondes.")
-    st.write(f"Dernière actualisation : {pd.to_datetime(station_information.get('last_updated'), unit='s')}")
+    try:
+        last_update_timestamp = pd.to_datetime(station_information.get("last_updated"), unit="s")
+        st.write(f"Dernière actualisation : {last_update_timestamp}")
+    except:
+        pass
 
-
-station_name = st.selectbox("Choisir une station", station_information_df["name"])
-station_id = station_information_df[station_information_df["name"] == station_name]["station_id"].values[0]
+station_information_df_names = station_information_df["name"].apply(get_language_text)
+station_name = st.selectbox("Choisir une station", station_information_df_names)
+station_id = station_information_df[station_information_df_names == station_name]["station_id"].values[0]
 
 station_information_selected = station_information_df[station_information_df["station_id"] == station_id]
 station_status_selected = station_status_df[station_status_df["station_id"] == station_id]
@@ -87,12 +123,14 @@ station_status_selected = station_status_df[station_status_df["station_id"] == s
 
 col1, col2 = st.columns(2)
 with col1:
-    st.write(f"Vous avez choisi la station **{station_name}** (id : {station_id})")
+    st.write(f"Vous avez choisi la station **{station_name.strip()}** (id : {station_id})")
 with col2:
     if "address" in station_information_selected.columns:
-        address  = station_information_selected["address"].values[0]
+        address = station_information_selected["address"].values[0]
         st.write(f"Adresse : {address}")
-    st.write(f"Coordonnées : {station_information_selected['lat'].values[0]}, {station_information_selected['lon'].values[0]}")
+    latitude = station_information_selected["lat"].values[0]
+    longitude = station_information_selected["lon"].values[0]
+    st.write(f"Coordonnées : {latitude}, {longitude}")
 
 col1, col2, col3 = st.columns(3)
 with col1:
